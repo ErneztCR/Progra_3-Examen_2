b0eba3e7dacce941107fcec624c48ac0cc669d61
ErneztCR <49740650+ErneztCR@users.noreply.github.com>
2023-11-16 21:55:21 -0600

    Menu

--- clases/ClaseUsuarios.cs
+++ clases/ClaseUsuarios.cs
@@ -7,5 +7,22 @@ namespace TallerDeReparaciones.clases
 {
     public class ClaseUsuarios
     {
+        // Variables
+        public static int UsuarioID { get; set; }
+        public static string Nombre { get; set; }
+        public static string CorreoElectronico { get; set; }
+        public static string Telefono { get; set; }
+
+        // Constructor
+        public ClaseUsuarios(int usuarioID, string nombre, string correoElectronico, string telefono)
+        {
+            UsuarioID = usuarioID;
+            Nombre = nombre;
+            CorreoElectronico = correoElectronico;
+            Telefono = telefono;
+        }
+
+        public ClaseUsuarios() { }
+
     }
 }
